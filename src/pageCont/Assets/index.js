7db1091eb5e7d54c841cae8af62ff87f34f7be9b
zrr <1521019968@qq.com>
2024-05-20 15:03:48 +0800

    fix:Inventory&Assets页面响应式调整

--- src/pageCont/Assets/index.js
+++ src/pageCont/Assets/index.js
@@ -3,8 +3,8 @@ import { withRouter } from 'next/router';
 import classNames from 'classnames';
 import styles from './index.module.scss';
 import Head from 'next/head';
-import AssetsTable from './Table'
-import CreateModal from './Modal/CreateModal'
+import AssetsTable from './Table';
+import CreateModal from './Modal/CreateModal';
 
 import {
   Button,
@@ -24,23 +24,23 @@ class Comp extends Component {
       assetType: '',
       responsiblePerson: '',
     },
-    isSearchClicked:false, //是否搜索
-    refresh:{},
-    createModalIsopen:false,//create modal 状态
-    editModalIsopen:false,//edit modal 状态
+    isSearchClicked: false, //是否搜索
+    refresh: {},
+    createModalIsopen: false, //create modal 状态
+    editModalIsopen: false, //edit modal 状态
   };
 
-  changeState = (obj)=>{
-    this.setState(obj)
-  }
+  changeState = (obj) => {
+    this.setState(obj);
+  };
 
   initFormValue = () => {
     this.setState({
       formValue: {
         assetId: '',
-      assetName: '',
-      assetType: '',
-      responsiblePerson: '',
+        assetName: '',
+        assetType: '',
+        responsiblePerson: '',
       },
       isSearchClicked: false,
     });
@@ -55,7 +55,8 @@ class Comp extends Component {
     });
   };
   render() {
-    const { formValue, isSearchClicked,createModalIsopen,editModalIsopen} = this.state;
+    const { formValue, isSearchClicked, createModalIsopen, editModalIsopen } =
+      this.state;
     return (
       <div>
         <Head>Assets</Head>
@@ -83,8 +84,8 @@ class Comp extends Component {
             onClick={() => {
               // setCreateModalOpen(true);
               this.setState({
-                createModalIsopen:true,
-              })
+                createModalIsopen: true,
+              });
             }}
             isExpressive
             size="md"
@@ -94,76 +95,82 @@ class Comp extends Component {
           </Button>
         </div>
         {/* 搜索框 */}
-        <div className="flex mt-12 space-x-4 items-end">
-          <TextInput
-            className="flex-auto w-20"
-            labelText="Asset ID"
-            id="assetId"
-            placeholder="Asset ID"
-            value={formValue.assetId}
-            onChange={(e) => {
-              this.FormValueChange(e);
-            }}
-          />
-          <TextInput
-            className="flex-auto w-20"
-            labelText="Asset"
-            id="assetName"
-            placeholder="Asset"
-            value={formValue.assetName}
-            onChange={(e) => {
-              this.FormValueChange(e);
-            }}
-          />
-          <TextInput
-            className="flex-auto w-20"
-            labelText="Type"
-            id="assetType"
-            placeholder="Type"
-            value={formValue.assetType}
-            onChange={(e) => {
-              this.FormValueChange(e);
-            }}
-          />
-          <TextInput
-            className="flex-auto w-20"
-            labelText="Person"
-            id="responsiblePerson"
-            placeholder="Person"
-            value={formValue.responsiblePerson}
-            onChange={(e) => {
-              this.FormValueChange(e);
-            }}
-          />
-          <Button
-            onClick={() => {
-              this.setState({
-                isSearchClicked: true,
-              })
-              //搜索
-              console.log(this.state.formValue, 'formValue');
-            }}
-            style={{ backgroundColor: '#393939' }}
-            isExpressive
-            size="md"
-            renderIcon={Search}
-            iconDescription="Search"
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => {
-              //清空搜索条件
-              this.initFormValue();
-            }}
-            style={{ backgroundColor: '#C6C6C6' }}
-            isExpressive
-            size="md"
-            renderIcon={Close}
-            iconDescription="Close"
-          >
-            Cancel
-          </Button>
+        <div className={styles.searchInputParent}>
+          <div className={styles.searchInput}>
+            <TextInput
+              className="flex-auto w-20"
+              labelText="Asset ID"
+              id="assetId"
+              placeholder="Asset ID"
+              value={formValue.assetId}
+              onChange={(e) => {
+                this.FormValueChange(e);
+              }}
+            />
+            <TextInput
+              className="flex-auto w-20"
+              labelText="Asset"
+              id="assetName"
+              placeholder="Asset"
+              value={formValue.assetName}
+              onChange={(e) => {
+                this.FormValueChange(e);
+              }}
+            />
+          </div>
+          <div className={styles.searchInput}>
+            <TextInput
+              className="flex-auto w-20"
+              labelText="Type"
+              id="assetType"
+              placeholder="Type"
+              value={formValue.assetType}
+              onChange={(e) => {
+                this.FormValueChange(e);
+              }}
+            />
+            <TextInput
+              className="flex-auto w-20"
+              labelText="Person"
+              id="responsiblePerson"
+              placeholder="Person"
+              value={formValue.responsiblePerson}
+              onChange={(e) => {
+                this.FormValueChange(e);
+              }}
+            />
+          </div>
+          <div className={styles.searchBtnParent}>
+            <Button
+              onClick={() => {
+                this.setState({
+                  isSearchClicked: true,
+                });
+                //搜索
+                console.log(this.state.formValue, 'formValue');
+              }}
+              style={{ backgroundColor: '#393939' }}
+              isExpressive
+              size="md"
+              renderIcon={Search}
+              iconDescription="Search"
+            >
+              Search
+            </Button>
+            <Button
+              onClick={() => {
+                //清空搜索条件
+                this.initFormValue();
+              }}
+              style={{ backgroundColor: '#C6C6C6' }}
+              isExpressive
+              size="md"
+              renderIcon={Close}
+              iconDescription="Close"
+            >
+              Cancel
+            </Button>
+          </div>
         </div>
         {/* table 表格 */}
         <div className="mt-12">
@@ -177,11 +184,12 @@ class Comp extends Component {
         </div>
 
         {/* Create a Asset modal */}
-        {<CreateModal
-          createModalIsopen = {createModalIsopen}
-          changeState = {this.changeState}
-        />}
-        
+        {
+          <CreateModal
+            createModalIsopen={createModalIsopen}
+            changeState={this.changeState}
+          />
+        }
       </div>
     );
   }
